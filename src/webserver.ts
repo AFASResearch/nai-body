126604c721076a847d14b297a5b7ec690a44078c
Johan Gorter <johan@johangorter.com>
2017-11-24 23:16:56 +0100

    emotion from webserver

--- src/webserver.ts
+++ src/webserver.ts
@@ -2,13 +2,28 @@ import { MessageLogic } from './logic/message-logic';
 import { Server } from 'http';
 import * as WebSocket from 'ws';
 import * as fs from 'fs';
+import {MicroBitService} from './services/microBit';
+var querystring = require('querystring');
 
 interface WebserverConfig {
   port: number;
 }
 
-export let startWebserver = (config: WebserverConfig, dependencies: {messageLogic: MessageLogic}) => {
+export let startWebserver = (config: WebserverConfig, dependencies: {messageLogic: MessageLogic, microBit: MicroBitService}) => {
   let server = new Server((req, res) => {
+    if (req.url === '/emotion') {
+      let body = '';
+      req.on('data', function (data) {
+        body += data;
+      });
+      req.on('end', () => {
+        let post = querystring.parse(body);
+        dependencies.microBit.sendCommand(post.emotion);
+        res.writeHead(302, { Location: '/index.html' });
+        res.end();
+      });
+      return;
+    }
     let indexHtml = fs.readFileSync(`${__dirname}/../public/index.html`, { encoding: 'UTF8' });
     res.writeHead(200, { type: 'text/html' });
     res.write(indexHtml);
